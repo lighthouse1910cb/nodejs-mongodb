2751f661a3ddf39aa68d7e3da2891f83cf7776ac
lighthouse1910cb <lighthouse1910cb@outlook.com>
2020-09-06 23:16:59 +0700

    add token return on res

--- controllers/userController.js
+++ controllers/userController.js
@@ -49,10 +49,9 @@ const register_post = async (req, res) => {
     const user = await User.create({ email, password });
     const token = createToken(user._id);
     res.cookie('jwt', token, {
-      httpOnly: true,
       maxAge: tokenExpiredTime * 1000,
     });
-    res.status(201).json({ user: user._id, isSuccess: true });
+    res.status(201).json({ user: user._id, isSuccess: true, token });
   } catch (error) {
     const errors = handleErrors(error);
     res.status(400).json({ errors, isSuccess: false });
@@ -66,10 +65,9 @@ const login_post = async (req, res) => {
       const user = await User.login({ email, password });
       const token = createToken(user._id);
       res.cookie('jwt', token, {
-        httpOnly: true,
         maxAge: tokenExpiredTime * 1000,
       });
-      res.status(200).json({ user: user._id, isSuccess: true });
+      res.status(200).json({ user: user._id, isSuccess: true, token });
     } catch (error) {
       const errors = handleErrors(error);
       res.status(400).json({ errors, isSuccess: false });
